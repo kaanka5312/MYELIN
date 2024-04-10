d2d310b4518644272d6ab25a5bb82a86d82cf178
kaanka5312 <kaanka5312@gmail.com>
2024-04-10 20:17:07 +0300

    Update MediationGLMM.R

--- R/MediationGLMM.R
+++ R/MediationGLMM.R
@@ -1,10 +1,23 @@
 test <- readMat("C:/Users/kaan/Documents/NatComm2023/MYELIN/DATA/MED.mat")
-d_2<- data.frame( GS_std = test$MED[,1] / max(test$MED[,1]) ,
+d_2<- data.frame( GS_std = standardize(test$MED[,1]) ,
                   ACW_std = standardize(test$MED[,2]) ,
                   MY_std = standardize(test$MED[,3]),
                   G = as.factor(test$MED[,5]))
+# Synthetic data
 
-mediated<- ulam( 
+
+mediated_1<- ulam( 
+  alist( 
+    GS_std ~ dnorm( mu , sigma ) , 
+    mu <- a[G] + bACW[G] * ACW_std + bMY[G] * MY_std ,
+    a[G] ~ dnorm( 0, 0.15 ) , 
+    bACW[G] ~ dnorm( 0 , 0.27 ) ,
+    bMY[G] ~ dnorm( 0 , 0.27 ) ,
+    sigma ~ dexp( 1 ) ) , 
+  data=d_2, chains = 4, cores = 4, log_lik = TRUE) 
+
+
+mediated_2<- ulam( 
   alist( 
     GS_std ~ dnorm( mu , sigma ) , 
     # Interaction term is in the end
@@ -15,3 +28,5 @@ mediated<- ulam(
     bACWMY[G] ~ dnorm( 0, 0.25 ),
     sigma ~ dexp( 1 ) ) , 
   data=d_2, chains = 4, cores = 4, log_lik = TRUE) 
+
+xseq <- c(-2,2) 
